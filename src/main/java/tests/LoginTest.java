3fd8fcc4748c0970430800144c43df83a229b8b5
alexandru.drop <alexandru.drop@gmail.com>
2024-05-24 18:04:17 +0300

    curs24 final

---
+++ src/main/java/tests/LoginTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import framework.utils.PropertiesFileProcessor;
+import pageObjects.LoginPage;
+import pageObjects.MenuPage;
+import selenium.utils.BaseTest;
+
+public class LoginTest extends BaseTest{
+	
+	String USER = PropertiesFileProcessor.readPropertiesFile("user", "credentials.properties");
+	String PASS = PropertiesFileProcessor.readPropertiesFile("pass", "credentials.properties");
+	
+	@Test(priority=1)
+	public void loginTest() {
+		
+		MenuPage menu = new MenuPage(driver);
+		LoginPage login = new LoginPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		login.logInApp(USER, PASS);
+		assertTrue(login.checkMsgIsDisplayed(login.successLoginMsg));
+		login.logoutFromApp();
+	}
+	
+	
+	@Test(priority=2)
+	public void invalidLoginTest() {
+		
+		MenuPage menu = new MenuPage(driver);
+		LoginPage login = new LoginPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		login.logInApp(USER, "123123");
+		assertTrue(login.checkMsgIsDisplayed(login.errorLoginMsg));
+	}
+
+}
