cac6c24b3dd570bfdc70a558f8373a17787fd629
alexandru.drop <alexandru.drop@gmail.com>
2024-05-24 16:07:11 +0300

    curs24

---
+++ src/main/java/selenium/utils/BaseTest.java
@@ -0,0 +1,33 @@
+package selenium.utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+	
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		//FirefoxDriver()
+		//EdgeDriver()
+		//System.set.property("webdriver.chrome.driver", path to cromedriver.exe")
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://keybooks.ro/");
+				
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000); //bad practice
+		//driver.close(); //inchide tabul curent
+		driver.quit(); // inchide fereastra de browser
+	}
+
+}
